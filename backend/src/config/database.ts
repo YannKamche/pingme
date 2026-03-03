bd7cd6cacb4b48733e9909a447fd8732c051b4a2
YannKamche <techwithyann@gmail.com>
2026-03-03 04:59:13 +0100

    setting mongodb connection to the app

---
+++ backend/src/config/database.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI as string);
+        console.log("✅ MongoDB connected successfully");
+    } catch (error) {
+        // This shows the error itself
+        console.error("❌MongoDB connection error:", error);
+
+        process.exit(1) // exit with failure
+        // status code 1 means failure
+        // status code 0 means success
+    }
+}
